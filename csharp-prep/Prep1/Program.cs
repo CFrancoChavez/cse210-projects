084fa907ee215ff89fc31234bfe2ceca32ceccee
CFrancoChavez <franco.chvez@gmail.com>
2023-01-07 07:56:31 -0300

    corrections

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,11 +7,11 @@ class Program
         Console.WriteLine("Hello Prep1 World!");
 
         Console.WriteLine("What is your first name?");
-        string FirstName = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.WriteLine("What is your last name?");
-        string LastName = Console.ReadLine();
-        
-        Console.Write($"Your name is {LastName}, {FirstName} {LastName}.");
+        string lastName = Console.ReadLine();
+
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
