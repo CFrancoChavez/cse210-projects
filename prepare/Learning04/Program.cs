98d3ccdb0ef4fc57f9c4c0223ad6ab60018b4104
CFrancoChavez <franco.chvez@gmail.com>
2023-02-25 10:04:44 -0300

    Practice inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        
+        Assignment assignment = new Assignment("Cesar Rebosio","Fractions");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("John Doe", "Algebra","5.9","6-13");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "American History", "The Causes of Independence War");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
